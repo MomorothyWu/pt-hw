09facbdefec790cf2874620c323a540bee42fc6e
MomorothyWu <gifted2815@gmail.com>
2025-06-12 18:13:25 +0800

    0612_share_success

--- src/app/share/result1/page.js
+++ src/app/share/result1/page.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Image from 'next/image';
+import result1 from '@/../public/4.result/result-1.png';
 
 export default function ShareResult1() {
   return (
     <>
       <Image className="flex justify-center w-[260px] h-auto mb-[16px]" src={result1} alt='result1' />
-
     </>
   );
 }
